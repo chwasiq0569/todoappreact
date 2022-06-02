9442a488c5a352877ba8db7ab4b6b0eda1b485c1
chwasiq0569 <wasiqabdullah222@gmail.com>
2022-06-02 09:28:30 +0500

    Update Page added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import CreatePage from "./pages/CreatePage/CreatePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UpdatePage from "./pages/UpdatePage/Updatepage";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/update" element={<UpdatePage />} />
       </Routes>
       {/* <Home /> */}
     </div>
